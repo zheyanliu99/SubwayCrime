c246f65f0237e5561d6e85e9c7b5cab5ed7f343a
zheyanliu99 <zl3119@columbia.edu>
2021-11-16 00:09:32 -0500

    Add column of Subway service and begin visualize Passenger by location

--- data/Subway_info.py
+++ data/Subway_info.py
@@ -52,24 +52,8 @@ class configure_googlemap():
 
         return self.address_list
 
-    # def configure_address(self, alist):
-    #     self.address_list['google_adress'].append(alist[0]['address_components'][0]['long_name'])
-    #     self.address_list['street_number'].append(alist[0]['address_components'][1]['long_name'])
-    #     self.address_list['route'].append(alist[0]['address_components'][2]['long_name'])
-    #     self.address_list['sublocality'].append(alist[0]['address_components'][3]['long_name'])
-    #     self.address_list['administrative_area_level_1'].append(alist[0]['address_components'][5]['long_name'])
-    #     self.address_list['administrative_area_level_2'].append(alist[0]['address_components'][6]['long_name'])
-    #     self.address_list['country'].append(alist[0]['address_components'][7]['long_name'])
 
-    #     self.address_list['formatted_address'].append(geocode_result[0]['formatted_address'])
-    #     self.address_list['lat'].append(alist[0]['geometry']['location']['lat'])
-    #     self.address_list['long'].append(alist[0]['geometry']['location']['lng'])
-    #     self.address_list['type'].append(alist[0]['types'])
-
-    #     return self.address_list
-
-# %% Use Google map api
-
+# %% Use Google map api get location info 
 
 # Read subway station df
 df = pd.read_csv('passenger_imputed.csv').drop_duplicates(subset=['station', 'linename'])[['station', 'linename']]
@@ -94,3 +78,60 @@ df2 = pd.DataFrame.from_dict(amap.address_list)
 df = df.reset_index(drop=True)
 df2 = df2.reset_index(drop=True)
 pd.concat([df, df2], axis=1).to_csv('subway_info_final3.csv', index = False)
+
+
+# %% create column subway service
+
+# read data
+df = pd.read_csv('subway_info_final3.csv')
+
+service_dict = {
+    '8 Avenue(ACE)':0,
+    'Shuttle(S)':0,
+    '6 Avenue(BDFM)':0,
+    'Brooklyn-Queens Crosstown(G)':0,
+    '14 St-Canarsie(L)':0,
+    'Broadway(NQRW)':0,
+    '7 Avenue(123)':0,
+    'Lexington Av(456)':0,
+    'Flushing(7)':0
+}
+
+def map_service(lines):
+
+    service_dict = {
+    '8 Avenue(ACE)':0,
+    'Shuttle(S)':0,
+    '6 Avenue(BDFM)':0,
+    'Brooklyn-Queens Crosstown(G)':0,
+    '14 St-Canarsie(L)':0,
+    'Broadway(NQRW)':0,
+    '7 Avenue(123)':0,
+    'Lexington Av(456)':0,
+    'Flushing(7)':0
+    }
+
+    for line in lines:
+        if line in 'ACE':
+            service_dict['8 Avenue(ACE)'] += 1
+        elif line in 'S':
+            service_dict['Shuttle(S)'] += 1
+        elif line in 'BDFM':
+            service_dict['6 Avenue(BDFM)'] += 1
+        elif line in 'G':
+            service_dict['Brooklyn-Queens Crosstown(G)'] += 1
+        elif line in 'L':
+             service_dict['14 St-Canarsie(L)'] += 1
+        elif line in 'NQRW':
+            service_dict['Broadway(NQRW)'] += 1
+        elif line in '123':
+            service_dict['7 Avenue(123)'] += 1
+        elif line in '456':
+            service_dict['Lexington Av(456)'] += 1
+        elif line in '7':
+            service_dict['Flushing(7)'] += 1
+    return max(service_dict, key = service_dict. get)
+
+df['service'] = [map_service(a) for a in df['linename']]
+
+df.to_csv('subway_info_final3.csv', index = False)
