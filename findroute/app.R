18c017461169d1c4efc20d08077afb4e41c875fe
zheyanliu99 <zl3119@columbia.edu>
2021-11-19 18:27:10 -0500

    Check out the new Shiny app, still need to work on the map

--- findroute/app.R
+++ findroute/app.R
@@ -21,12 +21,23 @@ mygoogle_routes = google_routes()
 mygoogle_routes$start_location = '168 st, New York'
 mygoogle_routes$destination = 'Prospect Park, New York'
 df = mygoogle_routes$get_directions() # it is a R dataframe
+# rownames(df) = df %>% pull(route_num)
 
+df %>% 
+  group_by(route_num) %>% 
+  # mutate(line = paste0(line, num_stops, collapse = '(')) %>% 
+  summarise(time = mean(time),
+            distance = mean(distance),
+            walking_distance = mean(walking_distance),
+            line = paste0(line, '[', as.character(num_stops), ']', collapse = " - ")) %>% 
+  rename('line[stops]' = line)
 
+df %>% 
+  mutate(line = paste0(line, num_stops, collapse = '('))
 ####################################
 # User Interface                   #
 ####################################
-ui <- fluidPage(theme = shinytheme("superhero"),
+ui <- fluidPage(theme = shinytheme("paper"),
                 navbarPage("No crime Navigation:",
                            
                            tabPanel("Home",
@@ -43,9 +54,9 @@ ui <- fluidPage(theme = shinytheme("superhero"),
                                     ),
                                     
                                     mainPanel(
-                                      tags$label(h3('Status/Output')), # Status/Output Text Box
+                                      tags$label(h3('Routes')), # Status/Output Text Box
                                       verbatimTextOutput('contents'),
-                                      tableOutput('tabledata'), # Results table
+                                      DT::dataTableOutput("tabledata"), # Results DT table
                                       tableOutput('tabledata2') # Results table
                                     ) # mainPanel()
                                     
@@ -61,12 +72,23 @@ ui <- fluidPage(theme = shinytheme("superhero"),
 server <- function(input, output, session) {
   
   # Input Data
+    
   datasetInput <- reactive({  
     
     mygoogle_routes = google_routes()
     mygoogle_routes$start_location = input$start_location
     mygoogle_routes$destination = input$destination
     df = mygoogle_routes$get_directions() # it is a R dataframe
+    df = 
+      df %>% 
+      group_by(route_num) %>% 
+      # mutate(line = paste0(line, num_stops, collapse = '(')) %>% 
+      summarise(time = mean(time),
+                distance = mean(distance),
+                walking_distance = mean(walking_distance),
+                line = paste0(line, '[', as.character(num_stops), ']', collapse = " - ")) %>% 
+      distinct(line, .keep_all = TRUE) %>% 
+      rename('line[stops]' = line)
     print(df)
     
   })
@@ -74,19 +96,27 @@ server <- function(input, output, session) {
   # Status/Output Text Box
   output$contents <- renderPrint({
     if (input$submitbutton>0) { 
-      isolate("Calculation complete.") 
+      isolate("Find results") 
     } else {
-      return("Please enter your staring location and destination")
+      return("Please enter your start location and destination")
     }
   })
   
   # Prediction results table
-  output$tabledata <- renderTable({
-    if (input$submitbutton>0) { 
-      datasetInput()
-    } 
-  })
+  # output$tabledata <- renderTable({
+  #   if (input$submitbutton>0) { 
+  #     datasetInput()
+  #   } 
+  # })
   
+  output$tabledata <- DT::renderDataTable({
+      if (input$submitbutton>0) {
+        DT::datatable(datasetInput(),
+                      options = list(scrollX = TRUE),
+                      rownames = FALSE)
+      }
+    })
+
 }
 
 
@@ -94,3 +124,4 @@ server <- function(input, output, session) {
 # Create Shiny App                 #
 ####################################
 shinyApp(ui = ui, server = server) 
+
